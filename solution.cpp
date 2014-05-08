7c1a9818dd38b73afd01ff6b414f08f1e2c216f1
Nicolas Pinchaud <nicolas.pinchaud@gmail.com>
2014-05-08 23:13:47 +0200

    Create solution.cpp

---
+++ solution.cpp
@@ -0,0 +1,22 @@
+// you can also use includes, for example:
+#include <algorithm>
+#include <iostream>
+
+bool Greater(int a, int b)
+{
+    return abs(a) < abs(b);
+}
+
+int solution(vector<int> &A) {
+    // write your code in C++98
+  std::make_heap (A.begin(),A.end(),Greater);
+  //This sorting algorithm has o(Nlog(N)) time complexity and o(1) space complexity
+  std::sort_heap (A.begin(),A.end(),Greater);
+
+  unsigned long min = abs(A[0] + A[0]);
+  for(int i=0; i<A.size()-1;++i) {
+      if(abs(A[i]+A[i+1])<min)
+        min = abs(A[i]+A[i+1]);
+  }
+  return min;
+}
